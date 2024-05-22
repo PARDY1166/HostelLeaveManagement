f11b5b8b61de8f1583bf77d4fde88c2177faa6f1
Sudarshana V Sharma <svsccodes@gmail.com>
2024-05-22 19:21:59 +0530

    add Warden.js

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+
+const corsOptions = { origin: "*" };
+app.use(cors(corsOptions));
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello to the api");
+});
+app.listen(3000, () => console.log(`app listening on port 3000...`));
