fb74e2faacddcbf201fd58361642305144d91c4d
PARDY1166 <puli.cs21@bmsce.ac.in>
2024-05-23 18:09:35 +0530

    frontend setup and appbar addition

---
+++ frontend/src/components/AppBar.jsx
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom"
+
+export default function Appbar({user}){
+    const navigate = useNavigate();
+    const link = (address)=>{
+        navigate("/"+address);
+    }
+    return(
+        <div className="flex justify-between items-center p-5 shadow">
+            <button className="font-bold text-xl" >Bmseth Hostels</button>
+            <div className="flex items-center ">
+                <div className="mr-10 font-medium">Hello, User {user}</div>
+                <button className="bg-gray-300 rounded-full w-10 h-10">U</button>
+            </div>
+        </div>
+    )
+}
